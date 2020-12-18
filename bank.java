faef82a46e0a355dcc06748b3e788b06ea774c58
Tieran Dysart <tierandysart@gmail.com>
2020-12-18 08:06:45 -0700

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,15 @@
+package FirstBank;
+import java.util.Scanner;
+
+public class bank {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		Account myAccount = new Account("Tieran", 1234);
+		myAccount.showMenu();
+		
+
+	}
+
+}
